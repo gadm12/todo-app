980c1a7f76a27348cabe2918e2fd7115ab319d50
Mohamed Gad <gadm5336@gmail.com>
2025-10-24 17:31:31 -0500

    update Flask project structure (V2.0)

--- V2.0_Flask/app.py
+++ V2.0_Flask/app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 
 app=Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    #nullable: so user would not be able to create a blank task
+    content = db.Column(db.String(200),nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    data_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    
+    def __repr__(self):
+        return "<Task %r>" % self.id
 
 @app.route('/')
-def home():
+def index():
     
-    return render_template("home.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
+    
     
     app.run(debug=True)
