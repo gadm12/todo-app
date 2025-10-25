ff94f57e5a6ce1c751a8f57824693815043df2ff
Mohamed Gad <gadm5336@gmail.com>
2025-10-25 17:03:18 -0500

    Add Flask app structure, Todo model, HTML layout, CSS table styling

--- V2.0_Flask/app.py
+++ V2.0_Flask/app.py
@@ -13,17 +13,27 @@ class Todo(db.Model):
     #nullable: so user would not be able to create a blank task
     content = db.Column(db.String(200),nullable=False)
     completed = db.Column(db.Integer, default=0)
-    data_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    date_created = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     
     def __repr__(self):
         return "<Task %r>" % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     
     return render_template("index.html")
 
 if __name__ == "__main__":
     
-    
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+    
+#inside python repl write the following to create the .db file
+#from app import app, db
+#with app.app_context():
+#    db.create_all()
+#hit enter again
+#to check the file path do the following inside python repl
+#import os
+#print(os.getcwd())
