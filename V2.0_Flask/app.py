e3fb6d3f6e899df5b23e477cef0f88ef0005670e
Mohamed Gad <gadm5336@gmail.com>
2025-10-26 13:37:15 -0500

    Implement full CRUD functionality with Flask and SQLAlchemy

--- V2.0_Flask/app.py
+++ V2.0_Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for,session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 
@@ -18,16 +18,74 @@ class Todo(db.Model):
     def __repr__(self):
         return "<Task %r>" % self.id
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def index():
     
-    return render_template("index.html")
+    if request.method == "POST":
+        task_content = request.form["content"]
+        new_task=Todo(content=task_content)
+    
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect(url_for("index"))
+        
+        except Exception as e:
+            print(f"Error adding task: {e}")
+            return "There was an issue adding your task"
+        
+    
+    else:
+        tasks=Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:id>")
+
+def delete(id):
+    
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect(url_for("index"))
+    except Exception as e:
+        print(f"Error deleting a task: {e}")
+        return "There was an issue deleting your task"
+    
+@app.route("/update/<int:id>",methods=["POST", "GET"])
+def update(id):
+    
+    task=Todo.query.get_or_404(id)
+    
+    if request.method == "POST":
+        task.content=request.form["content"]
+        
+        
+    
+        try:
+    
+            db.session.commit()
+            return redirect(url_for("index"))
+        
+        except Exception as e:
+            print(f"Error updating task: {e}")
+            return "There was an issue with updating your task"
+    
+    else:
+        return render_template("update.html", task=task)
+    
 
 if __name__ == "__main__":
     
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+    
+    
+    
+    
+    
     
 #inside python repl write the following to create the .db file
 #from app import app, db
